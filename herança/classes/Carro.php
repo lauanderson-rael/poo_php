7927e6ed8f4f5af79bca269bae0aaf37d01f229d
lauanderson-rael <lauanderson38@gmail.com>
2025-02-22 01:32:29 -0300

    commit 2

--- herança/classes/Carro.php
+++ herança/classes/Carro.php
@@ -1,6 +1,16 @@
 <?php
 
 class Carro extends Veiculo{
+    private $portas;
+
+    function __construct(string $nome, string $modelo, int $qtdPassageiros, int $portas){
+        parent::__construct($nome, $modelo, $qtdPassageiros);
+        $this->portas = $portas;
+    }
+
+    public function getPortas(){
+        return $this->portas;
+    }
 
     public function acelerar(){
         echo "O carro esta acelerando\n";
--- herança/index.php
+++ herança/index.php
@@ -4,8 +4,9 @@ require_once('./classes/Veiculo.php');
 require_once('./classes/Carro.php');
 require_once('./classes/Bicicleta.php');
 
-$c1 = new Carro('Ford', 'Fiesta', 4);
+$c1 = new Carro('Ford', 'Fiesta', 5, 4);
 echo "O veiculo: {$c1->getNome()} - {$c1->getModelo()}, suporta {$c1->getQtdPassageiros()} passageiros\n";
+echo "O carro tem {$c1->getPortas()} portas\n";
 $c1->acelerar();
 echo "-----------------------\n";
 
